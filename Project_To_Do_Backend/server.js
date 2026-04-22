076ac261196c862e3f3af8696e5e20dfcc5b63d0
Shubhammodi7 <136661469+Shubhammodi7@users.noreply.github.com>
2026-04-22 16:52:53 +0530

    Change server listen address to '0.0.0.0'

--- Project_To_Do_Backend/server.js
+++ Project_To_Do_Backend/server.js
@@ -25,6 +25,7 @@ app.use('/api/user', userRoutes);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-})
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
